b3e06c4c7a3ef4fd326c899295008d17cce2c024
Stephen Crim <stephencrim@gmail.com>
2014-09-07 14:48:56 -0600

    SHIPPING IT UP THE HILL

--- newGenerator.ino
+++ newGenerator.ino
@@ -1,16 +1,20 @@
 #include <Ethernet.h>
-#include <util.h>
+//#include <util.h>
 #include <TimerOne.h>
 #include <SoftwareSerial.h>
 #include <WebServer.h>
+#include "SPI.h"
+
+P(ok) = "OK\r\n";
+
 /* Ethernet and Web Config */
 static uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-static uint8_t ip[] = { 10,0,1,25 };
+static uint8_t ip[] = { 10,0,0,25 };
 WebServer www("", 80);
 
 /* magic numbers */
-#define HISTLEN 5
-#define QUEUELEN 5
+#define HISTLEN 10
+#define QUEUELEN 10
 #define NOOP 0
 #define SPIN 1
 #define WAIT 2
@@ -22,7 +26,6 @@ struct pins_config {
   int ignition;
   int flowsensor;
   int panel;
-  int fakesensor;
 } pins;
 
 /* COMMAND QUEUE
@@ -41,7 +44,7 @@ typedef struct cmd {
 
 
 cmd noop;
-cmd last_command;
+cmd last;
   
 struct state {
   volatile int ticks;
@@ -50,11 +53,221 @@ struct state {
   int totalticks;
   cmd queue[QUEUELEN];
   int queueidx;
+  int queueadv;
   bool printState;
   bool printCmdQueue;
   bool collateFlow;
   bool doCommand;
 } g;
+
+
+/* web functions */
+void webIndex(WebServer &server, WebServer::ConnectionType type, char *, bool)
+{
+  server.httpSuccess();
+  if (type != WebServer::HEAD)
+  {
+    P(indexHTML) = "<html><a href='run' target='tgt'>Turn on Ignition</a> | <a href='start' target='tgt'>Start Engine</a> | <a href='stop' target='tgt'>Stop Engine</a><br/><iframe src='ready' name='tgt'/></html>";
+    server.printP(indexHTML);
+  }
+}
+
+void webPrintInt( WebServer &server, char* name, int x, int indent, int index ) {
+    for (int i=0; i<indent; i++){
+        server.print("\t");
+    }
+    server.print(name);
+    if (index != -1) {
+        server.print("[");
+        server.print(index);
+        server.print("]");
+    }
+    server.print(": ");
+    server.print(x);
+    server.print("\r\n");
+}
+
+void webPrintChr( WebServer &server, char* name, char* x, int indent, int index ) {
+    for (int i=0; i<indent; i++){
+        server.print("\t");
+    }
+    server.print(name);
+    if (index != -1) {
+        server.print("[");
+        server.print(index);
+        server.print("]");
+    }
+    server.print(": ");
+    server.print(x);
+    server.print("\r\n");
+}
+
+void webPrintPin( WebServer &server, char* name, int x, int indent, int index ) {
+    for (int i=0; i<indent; i++){
+        server.print("\t");
+    }
+    server.print(name);
+    if (index != -1) {
+        server.print("[");
+        server.print(index);
+        server.print("]");
+    }
+    server.print(": ");
+    server.print(x ? "HIGH" : "LOW");
+    server.print("\r\n");
+}
+
+void webPrintHist(WebServer &server, WebServer::ConnectionType type, char *, bool) {
+    server.httpSuccess();
+    webPrintInt( server, "g.histidx", g.histidx, 1, -1);
+    server.print("Flow History:\r\n");
+    for( int i = 0; i < HISTLEN; i++ ) {
+        webPrintInt( server, "g.tickhistory",g.tickhistory[i], 1, i);
+    }  
+}
+
+void webPrintState(WebServer &server, WebServer::ConnectionType type, char *, bool) {
+    server.httpSuccess();
+    webPrintPin( server, "pins.starter", digitalRead(pins.starter), 0,-1);
+    webPrintPin( server, "pins.ignition", digitalRead(pins.ignition), 0,-1);
+    webPrintPin( server, "pins.panel", digitalRead(pins.panel), 0,-1);
+    server.print("Flow Sensor:\r\n");
+    webPrintInt( server, "g.ticks", g.ticks, 1,-1);
+    webPrintInt( server, "g.totalticks", g.totalticks, 1,-1);  
+}
+
+void webPrintCmdQueue(WebServer &server, WebServer::ConnectionType type, char *, bool) {
+    server.httpSuccess();
+    webPrintInt( server, "g.queueidx", g.queueidx, 0, -1);
+    for ( int i = 0; i < QUEUELEN; i++ ) {
+        webPrintInt( server, "g.queue.inst", g.queue[i].inst, 1, i);
+        webPrintInt( server, "g.queue.arg1", g.queue[i].arg1, 1, i);
+        webPrintInt( server, "g.queue.arg2", g.queue[i].arg2, 1, i);
+  }
+  g.printCmdQueue = false;
+}
+
+void webFlushQueue(WebServer &server, WebServer::ConnectionType type, char *, bool) {
+    for (int i=0; i < QUEUELEN; i++) {
+      g.queue[i] = noop;
+  }
+  digitalWrite(pins.ignition, HIGH);
+  digitalWrite(pins.starter, HIGH);
+  
+  server.httpSuccess();
+  server.printP(ok);
+}
+
+void webPumpForHour(WebServer &server, WebServer::ConnectionType type, char *, bool){
+        if (digitalRead(pins.panel)) {
+        server.httpFail();
+        server.print("NOT OK: PANEL ALREADY HIGH\r\n");
+    } else {
+        server.httpSuccess();
+        addCommand(SPIN, pins.ignition, LOW);
+        addCommand(SPIN, pins.starter, LOW);
+        addCommand(WAIT, 3, -1);
+        addCommand(SPIN, pins.starter, HIGH);
+        addCommand(WAIT, 297, -1);
+        addCommand(SPIN, pins.ignition, HIGH);
+        addCommand(SPIN, pins.panel, LOW);
+        addCommand(WAIT, 60*15, -1);
+        addCommand(SPIN, pins.ignition, LOW);
+        addCommand(SPIN, pins.starter, LOW);
+        addCommand(WAIT, 3, -1);
+        addCommand(SPIN, pins.starter, HIGH);
+        addCommand(WAIT, 297, -1);
+        addCommand(SPIN, pins.ignition, HIGH);
+        addCommand(SPIN, pins.panel, LOW);
+        addCommand(WAIT, 60*15, -1);
+        addCommand(SPIN, pins.ignition, LOW);
+        addCommand(SPIN, pins.starter, LOW);
+        addCommand(WAIT, 3, -1);
+        addCommand(SPIN, pins.starter, HIGH);
+        addCommand(WAIT, 297, -1);
+        addCommand(SPIN, pins.ignition, HIGH);
+        addCommand(SPIN, pins.panel, LOW);
+        addCommand(WAIT, 60*15, -1);
+        addCommand(SPIN, pins.ignition, LOW);
+        addCommand(SPIN, pins.starter, LOW);
+        addCommand(WAIT, 3, -1);
+        addCommand(SPIN, pins.starter, HIGH);
+        addCommand(WAIT, 297, -1);
+        addCommand(SPIN, pins.ignition, HIGH);
+        addCommand(SPIN, pins.panel, LOW);
+        addCommand(WAIT, 60*15, -1);
+        server.printP(ok); 
+    }
+}
+
+void webPrintStatus(WebServer &server, WebServer::ConnectionType type, char *, bool){
+    server.httpSuccess();
+    if (g.queue[g.queueidx].inst == NOOP) {
+        server.print("IDLE\r\n");
+    } else if (g.queue[g.queueidx].inst == WAIT) {
+        webPrintInt(server, "WAITING", g.queue[g.queueidx].arg1,0,-1);
+    } else if (g.queue[g.queueidx].inst == SPIN) {
+        webPrintInt(server, "SETTING PIN", g.queue[g.queueidx].arg2,0,g.queue[g.queueidx].arg1);
+    }
+}
+
+void webPump(WebServer &server, WebServer::ConnectionType type, char *, bool){
+    if (digitalRead(pins.panel)) {
+        server.httpFail();
+        server.print("NOT OK: PANEL ALREADY HIGH\r\n");
+    } else {
+        server.httpSuccess();
+        addCommand(SPIN, pins.ignition, LOW);
+        addCommand(SPIN, pins.starter, LOW);
+        addCommand(WAIT, 3, -1);
+        addCommand(SPIN, pins.starter, HIGH);
+        addCommand(WAIT, 297, -1);
+        addCommand(SPIN, pins.ignition, HIGH);
+        addCommand(SPIN, pins.panel, LOW);
+        server.printP(ok); 
+    }
+}
+
+void webCmd( WebServer &server, WebServer::ConnectionType type, char * url_tail, bool ) {
+  URLPARAM_RESULT rc;
+  #define NAMELEN 32
+  #define VALUELEN 32
+  
+  char name[NAMELEN];
+  char value[VALUELEN];
+  
+  //char name_a[3][NAMELEN];
+  char value_a[3][VALUELEN];
+  
+  int i = 0;
+  cmd c;
+  server.httpSuccess();
+  if (strlen(url_tail)) {
+    while (strlen(url_tail)) {
+     rc = server.nextURLparam(&url_tail, name, NAMELEN, value, VALUELEN);
+     if (rc == URLPARAM_EOS) {}
+        else {
+            //name_a[i] = name;
+            /*
+            printChr("name",name,0,-1);
+            printInt("name sizeof", sizeof(name), 0, -1);
+            printChr("val",value,0,-1);
+            printInt("val sizeof", sizeof(value), 0, -1);
+            */
+            strcpy(value_a[i],value);
+            i++;
+        }
+    }
+    /* time to check that insanity for appropriate input */
+    if (i != 3) {
+        printInt("BAD NEWS FOR URL PARAMS", i, 0, -1);
+    } else {
+        addCommand( atoi(&value_a[0][0]), atoi(&value_a[1][0]), atoi(&value_a[2][0]) );
+    }
+  }
+  P(navMsg) = "";
+  server.printP(navMsg);
+}
 
 
 void setup() {
@@ -64,15 +277,12 @@ void setup() {
   g.totalticks = 0;
   memset(g.tickhistory, -1, sizeof(g.tickhistory));
   g.queueidx = 0;
+  g.queueadv = 0;
   
   /* useful global commands */
   noop.inst = NOOP;
   noop.arg1 = -1;
   noop.arg2 = -1;
-  
-  last_command.isnt = LAST_COMMAND;
-  last_command.arg1 = -1;
-  last_command.arg2 = -1;
   
   for (int i=0; i < QUEUELEN; i++) {
       g.queue[i] = noop;
@@ -88,7 +298,6 @@ void setup() {
   pinMode(pins.ignition, OUTPUT);
   pinMode(pins.flowsensor, INPUT);
   pinMode(pins.panel, INPUT);
-  //pinMode(pins.fakesensor, INPUT_PULLUP);
   
   digitalWrite(pins.starter, HIGH);
   digitalWrite(pins.ignition, HIGH);
@@ -96,12 +305,23 @@ void setup() {
   /* interrupt setup */
   Timer1.initialize(1000000);
   Timer1.attachInterrupt(secondlyInterrupt);
+  
   // set to RISING for actual hall sensor.
   attachInterrupt(1, flowsensor, RISING);
-  addCommand(0, WAIT, 3, -1);
-  addCommand(1, SPIN, pins.starter, LOW);
-  addCommand(2, WAIT, 3, -1);
-  addCommand(3, SPIN, pins.starter, HIGH);
+
+  /* ethernet and web setup oh god now */
+  Ethernet.begin(mac, ip);
+  www.setDefaultCommand(&webIndex);
+  www.addCommand("index.html", &webIndex);
+  www.addCommand("cmd", &webCmd);
+  www.addCommand("pumpFor5", &webPump);
+  www.addCommand("pumpFor60", &webPumpForHour);
+  www.addCommand("cmdQueue", &webPrintCmdQueue);
+  www.addCommand("state", &webPrintState);
+  www.addCommand("status", &webPrintStatus);
+  www.addCommand("flush", &webFlushQueue);
+  www.addCommand("tickhist", &webPrintHist);
+  www.begin();
 }
   
 /* interrupt functions */
@@ -191,12 +411,17 @@ void collateFlow() {
   g.collateFlow = false;
 }
 
-void addCommand( int idx, int inst, int arg1, int arg2) {
+void insertCommand( int idx, int inst, int arg1, int arg2) {
     cmd c;
     c.inst = inst;
     c.arg1 = arg1;
     c.arg2 = arg2;
     g.queue[idx] = c;
+}
+
+void addCommand( int inst, int arg1, int arg2 ) {
+    insertCommand( g.queueadv, inst, arg1, arg2);
+    g.queueadv = (g.queueadv + 1) % QUEUELEN;
 }
 
 void doCommands() {
@@ -207,26 +432,35 @@ void doCommands() {
         Serial.println("Setting a pin...");
         digitalWrite(g.queue[g.queueidx].arg1, g.queue[g.queueidx].arg2);
         g.queue[g.queueidx] = noop;
-            g.queueidx = (g.queueidx + 1) % QUEUELEN;
+        advQueueIdx();
     } 
     if (g.queue[g.queueidx].inst == WAIT ){
         Serial.println("Waiting...");
         g.queue[g.queueidx].arg1--;
-        if (g.queue[g.queueidx].arg1 == 0) {
+        if (g.queue[g.queueidx].arg1 == 1) {
             g.queue[g.queueidx] = noop;
-            g.queueidx = (g.queueidx + 1) % QUEUELEN;
+            advQueueIdx();
         }
+        g.doCommand=false;
     }
-    g.doCommand=false;
+    
+}
+
+void advQueueIdx () {
+    if (g.queueadv == g.queueidx) {
+        g.queueadv = (g.queueadv + 1) % QUEUELEN;
+    }
+    g.queueidx = (g.queueidx + 1) % QUEUELEN;
 }
 
 void secondlyInterrupt() {
   g.printState = false;
   g.collateFlow = true;
-  g.printCmdQueue = true;
+  g.printCmdQueue = false;
   if (g.queue[g.queueidx].inst != NOOP){ 
       g.doCommand = true; 
   }
+  delay(1000);
 }
 
 /* procedural functions */
@@ -243,60 +477,10 @@ void loop () {
   if (g.doCommand) {
       doCommands();
   }
-  delay(100);
-}
+  char buff[64];
+  int len = 64;
 
-/* web functions */
-void webIndex(WebServer &server, WebServer::ConnectionType type, char *, bool)
-{
-  server.httpSuccess();
-  if (type != WebServer::HEAD)
-  {
-    P(indexHTML) = "<html><a href='run' target='tgt'>Turn on Ignition</a> | <a href='start' target='tgt'>Start Engine</a> | <a href='stop' target='tgt'>Stop Engine</a><br/><iframe src='ready' name='tgt'/></html>";
-    server.printP(indexHTML);
-  }
-}
-void webNav( WebServer &server, WebServer::ConnectionType type, char *, bool ) {
-  server.httpSuccess();
-  P(navMsg) = "";
-  server.printP(navMsg);
-}
-
-void webCmd( WebServer &server, WebServer::ConnectionType type, char * url_tail, bool ) {
-  URLPARAM_RESULT rc;
-  #define NAMELEN 32
-  #define VALUELEN 32
-  
-  char name[NAMELEN];
-  char value[VALUELEN];
-  
-  //char name_a[3][NAMELEN];
-  char value_a[3][VALUELEN];
-  
-  int i = 0;
-  cmd c;
-  server.httpSuccess();
-  if (strlen(url_tail)) {
-    while (strlen(url_tail)) {
-     rc = server.nextURLparam(&url_tail, name, NAMELEN, value, VALUELEN);
-     if (rc == URLPARAM_EOS) {}
-        else {
-            //name_a[i] = name;
-            strcpy(value_a[i],value);
-            i++;
-        }
-    }
-    /* time to check that insanity for appropriate input */
-    if (i != 2) {
-        printInt("BAD NEWS FOR URL PARAMS", i, 0, -1);
-    } else {
-        c.inst = int(value_a[0]);
-        c.arg1 = int(value_a[1]);
-        c.arg2 = int(value_a[2]);
-        
-    }
-  }
-  P(navMsg) = "";
-  server.printP(navMsg);
+  /* process incoming connections one at a time forever */
+  www.processConnection(buff, &len);
 }
 
